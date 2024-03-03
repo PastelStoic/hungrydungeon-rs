f33d2d527ecee0d90affe728cc46fb569070da96
PastelStoic <pastelstoic@proton.me>
2024-03-03 18:31:47 -0500

    Continued work

---
+++ design/ai/index.md
@@ -0,0 +1,6 @@
+All ai have a series of possible actions they can take, each of which has a way
+of producing a ranking, either 0 (action should not be taken) or a positive
+non-zero integer representing how strongly the ai wants to take the action.
+
+Examples
+- Basic attack. Requires the attack value of the attacking entity, as well as their Entity for possible retaliation.
--- src/actors/ai.rs
+++ src/actors/ai.rs
@@ -1,4 +1,29 @@
-use bevy::prelude::Component;
+use bevy::prelude::*;
+use rand::prelude::*;
+
+use super::Actor;
 
 #[derive(Component)]
-pub struct SlimeAi;
+pub struct Ai;
+
+#[derive(Event)]
+pub struct AttackActionEvent(Entity, Entity);
+
+pub fn run_ai(query: Query<(Entity, &Name, &Actor, Option<&Ai>)>) {
+    // has a list of possible actions based on AI type
+    // each of these actions is calculated, given a weight
+    // for now, skip all this, just find the nearest actor and attack
+    for l1 in &query {
+        if let Some(_) = l1.3 {
+            let mut possible_targets = vec![];
+            for l2 in &query {
+                if l1.0 != l2.0 {
+                    possible_targets.push((1, l2.0));
+                }
+            }
+
+            // picks target, creates attack event. Unique events for every possible action?
+            
+        }
+    }
+}
