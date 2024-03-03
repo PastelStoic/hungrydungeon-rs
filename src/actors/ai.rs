79a7d41cf58f4971c089d158393034fd604e4938
PastelStoic <pastelstoic@proton.me>
2024-03-03 18:51:36 -0500

    Finished rough ai concept

--- src/actors/ai.rs
+++ src/actors/ai.rs
@@ -9,7 +9,10 @@ pub struct Ai;
 #[derive(Event)]
 pub struct AttackActionEvent(Entity, Entity);
 
-pub fn run_ai(query: Query<(Entity, &Name, &Actor, Option<&Ai>)>) {
+pub fn run_ai(
+    query: Query<(Entity, &Name, &Actor, Option<&Ai>)>,
+    mut writer: EventWriter<AttackActionEvent>,
+) {
     // has a list of possible actions based on AI type
     // each of these actions is calculated, given a weight
     // for now, skip all this, just find the nearest actor and attack
@@ -23,7 +26,31 @@ pub fn run_ai(query: Query<(Entity, &Name, &Actor, Option<&Ai>)>) {
             }
 
             // picks target, creates attack event. Unique events for every possible action?
-            
+            if let Some(target) = possible_targets.first() {
+                writer.send(AttackActionEvent(l1.0, target.1));
+            }
+        }
+    }
+}
+
+pub fn respond_to_ai(
+    mut reader: EventReader<AttackActionEvent>,
+    mut query: Query<(Entity, &Name, &mut Actor, Option<&Ai>)>,
+) {
+    for ev in reader.read() {
+        let [attacker, mut target] = query.many_mut([ev.0, ev.1]);
+        // this works!
+        if target.2.health_current > 0 {
+            target.2.health_current -= attacker.2.attack;
+            println!(
+                "AI {} dealt {} damage to {}, leaving them with {} hp.",
+                attacker.1, attacker.2.attack, target.1, target.2.health_current
+            );
+        } else {
+            println!(
+                "AI {} sees {}, but their hp isn't high enough to attack.",
+                attacker.1, target.1,
+            );
         }
     }
 }
